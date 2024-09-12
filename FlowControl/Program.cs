ecad84d5983097c91cf74b51b690056427715fa2
Luna L <hanken95@gmail.com>
2024-09-12 19:50:56 +0200

    Add project files.

---
+++ FlowControl/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tools;
+
+namespace FlowControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exitLoop = false;
+            while (!exitLoop)
+            {
+                Console.WriteLine("Welcome to the main menu. You can navigate the main menu by pressing the corresponding" +
+                    " numbers to test the different functions");
+                Console.WriteLine("1: Movie ticket price calculator");
+                Console.WriteLine("2: Movie ticket price calculator for a group");
+                Console.WriteLine("0: Exit program");
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case '1':
+                        Console.WriteLine(CinemaTools.MovieTicketPriceCalculator());
+                        Console.WriteLine("\nPress any key to continue");
+                        Console.ReadKey(true);
+                        break;
+                    case '2':
+                        Console.WriteLine(CinemaTools.MovieTicketPriceCalculatorForGroup());
+                        Console.WriteLine("\nPress any key to continue");
+                        Console.ReadKey(true);
+                        break;
+                    case '0':
+                        exitLoop = true;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input. Press any key to try again");
+                        Console.ReadKey(true);
+                        break;
+                }
+                Console.Clear();
+            }
+        }
+
+
+        
+    }
+}
---
+++ Tools/CinemaTools.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools
+{
+    public enum PriceGroup
+    {
+        Youth = 80,
+        Retiree = 90,
+        Standard = 120
+    }
+    public enum PriceGroupMaxAge
+    {
+        YouthMaxAge = 19,
+        StandardMaxAge = 64
+    }
+
+    public static class CinemaTools
+    {
+        public static string MovieTicketPriceCalculator()
+        {
+            int age = AgeInputHandler();
+            Console.Clear();
+            if (age <= (int)PriceGroupMaxAge.YouthMaxAge)
+            {
+                return $"Youth price: {(int)PriceGroup.Youth}kr";
+            }
+            else if (age > (int)PriceGroupMaxAge.StandardMaxAge)
+            {
+                return $"Retiree price: {(int)PriceGroup.Retiree}kr";
+            }
+            else
+            {
+                return $"Standard price: {(int)PriceGroup.Standard}kr";
+            }
+        }
+
+        public static string MovieTicketPriceCalculatorForGroup()
+        {
+            Console.Clear();
+
+
+            int amountOfPeople = 0;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter the number of people that are gonna attend");
+                if (int.TryParse(Console.ReadLine(), out amountOfPeople))
+                {
+                    if (amountOfPeople > 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Cannot be less than 1 person");
+                        Console.WriteLine("Press a key to restart age input");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Wrong input. Press a key to restart age input");
+                    Console.ReadKey();
+                }
+            }
+
+
+            int totalPrice = 0;
+            foreach (int age in AgesOfGroupInputHandler(amountOfPeople))
+            {
+                if (age <= (int)PriceGroupMaxAge.YouthMaxAge)
+                {
+                    totalPrice += (int)PriceGroup.Youth;
+                }
+                else if (age > (int)PriceGroupMaxAge.StandardMaxAge)
+                {
+                    totalPrice += (int)PriceGroup.Retiree;
+                }
+                else
+                {
+                    totalPrice += (int)PriceGroup.Standard;
+                }
+            }
+
+            Console.Clear();
+
+            return $"Number of people: {amountOfPeople}" +
+                $"\nTotal price for everyone: {totalPrice}";
+        }
+
+        private static int AgeInputHandler()
+        {
+            int age = 0;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter the age of the person");
+                if (int.TryParse(Console.ReadLine(), out age))
+                {
+                    if (age >= 0 && age < 150)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Age can only be between 0 and 150");
+                        Console.WriteLine("Press a key to restart age input");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Wrong input. Press a key to restart age input");
+                    Console.ReadKey();
+                }
+            }
+
+            return age;
+        }
+        
+        private static List<int> AgesOfGroupInputHandler(int numberOfPeople)
+        {
+            List<int> ages = new List<int>();
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                int age;
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Enter the age of person number {i + 1}");
+                    if (int.TryParse(Console.ReadLine(), out age))
+                    {
+                        if (age >= 0 && age < 150)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Age can only be between 0 and 150");
+                            Console.WriteLine("Press a key to restart age input");
+                            Console.ReadKey();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Wrong input. Press a key to restart age input");
+                        Console.ReadKey();
+                    }
+                }
+                ages.Add(age);
+            }
+
+            return ages;
+        }
+    }
+}
